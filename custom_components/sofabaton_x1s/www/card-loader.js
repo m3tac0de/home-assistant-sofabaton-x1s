8b6d4620a74bdb3f8154191bc5a8452d6a4e4f68
m3tac0de <highvoltagecode@gmail.com>
2026-01-20 01:20:59 +0100

    Removed outdated logging.

--- custom_components/sofabaton_x1s/www/card-loader.js
+++ custom_components/sofabaton_x1s/www/card-loader.js
@@ -1,11 +1,5 @@
-console.warn("[cards-loader] loaded");
-
-// Works reliably in module context:
 const LOADER_URL = new URL(import.meta.url, window.location.href);
 const VERSION = LOADER_URL.searchParams.get("v") || "dev";
-
-console.warn("[cards-loader] LOADER_URL =", LOADER_URL.toString());
-console.warn("[cards-loader] VERSION =", VERSION);
 
 function withVersion(url) {
   const u = new URL(url, window.location.href);
@@ -26,10 +20,7 @@ function loadModuleScript(url) {
 
 async function boot() {
   const cardUrl = withVersion("/sofabaton_x1s/www/remote-card.js");
-  console.warn("[cards-loader] loading:", cardUrl);
-
   await loadModuleScript(cardUrl);
-
   document.dispatchEvent(new Event("ll-rebuild", { bubbles: true, composed: true }));
 }
 
