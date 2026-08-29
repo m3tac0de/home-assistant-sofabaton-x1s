55bcb2545313f278c8abfa52f8c6ff2ffc41ccda
m3tac0de <highvoltagecode@gmail.com>
2026-08-29 19:50:18 +0200

    version fix

--- custom_components/sofabaton_x1s/www/remote-card.js
+++ custom_components/sofabaton_x1s/www/remote-card.js
@@ -2773,7 +2773,7 @@ var REMOTE_CARD_EDITOR_CSS = `
 
 // custom_components/sofabaton_x1s/www/src/remote-card-shared.ts
 var CARD_NAME = "Sofabaton Virtual Remote";
-var CARD_VERSION = "0.4.0";
+var CARD_VERSION = "0.2.2";
 var KEY_CAPTURE_HELP_URL = "https://github.com/m3tac0de/sofabaton-virtual-remote/blob/main/docs/keycapture.md";
 var LOG_ONCE_KEY = `__${CARD_NAME}_logged__`;
 var AUTOMATION_ASSIST_SESSION_KEY = "__sofabatonAutomationAssistSession__";
